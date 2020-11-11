ffd7971f1c01b1949cd9197fcb04516045717f37
tibari1980 <tibarinewdzign@gmail.com>
2020-11-11 20:59:00 +0100

    config heruko page not found

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const path=require('path');
 const app=express();
 
 app.use(express.static(__dirname+'/dist/GestionClient'));
-
-app.all('*',(req,res)=>{
-    res.status(200).sendFile(__dirname+'/dist/GestionClient/index.html');
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/GestionClient/index.html'))
 });
+
+
+//app.all('*',(req,res)=>{
+//    res.status(200).sendFile(__dirname+'/dist/GestionClient/index.html');
+//});
 app.listen(process.env.PORT || 8080);
